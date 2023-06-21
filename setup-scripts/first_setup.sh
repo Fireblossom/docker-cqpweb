52c2ad366fbe8aa4c5e29e6100e0315e374cb160
Fireblossom <duanxu961112@gmail.com>
2023-06-21 15:02:32 +0200

    update set up script

--- setup-scripts/first_setup.sh
+++ setup-scripts/first_setup.sh
@@ -1,3 +1,6 @@
-python3 bootstrap.py
 ./run_cqp
-/opt/tljh/hub/bin/python -m pip install cwb-ccc
+python3 bootstrap.py --admin $CQPWEB_USER:$CQPWEB_USER_PASSWORD
+tljh-config set http.port 8080
+tljh-config reload proxy
+source /opt/tljh/user/bin/activate
+pip install cwb-ccc pandas plotly seaborn voila
